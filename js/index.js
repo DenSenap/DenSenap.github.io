f755e18a583383b0d3f21864dfe3f88fa91f59e6
densenap <32615739+DenMustard@users.noreply.github.com>
2017-11-04 06:23:39 +0100

    now sleep

--- js/index.js
+++ js/index.js
@@ -81,3 +81,134 @@ function hasScrolled() {
 
     lastScrollTop = st;
 }
+
+  $(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                        message:'Vänligen fyll i ditt förnamn'
+                    },
+                        notEmpty: {
+                        message: 'Vänligen fyll i ditt förnamn'
+                    }
+                }
+            },
+             efternamn: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                        message:'Vänligen fyll i ditt efternamn'
+                    },
+                    notEmpty: {
+                        message: 'Vänligen fyll i ditt efternamn'
+                    }
+                }
+            },
+             organisation: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                        message:'Vänligen fyll i vilken organisation du representerar'
+                    },
+                    notEmpty: {
+                        message: 'Vänligen fyll i vilken organisation du representerar'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Vänligen fyll i din e-postadress'
+                    },
+                    emailAddress: {
+                        message: 'Vänligen fyll i en giltig e-postadress'
+                    }
+                }
+            },
+             phone: {
+                validators: {
+                     stringLength: {
+                        min: 9,
+                        max: 10,
+                        message:'Vänligen fyll i ett giltigt telefonnummer'
+                    },
+                    notEmpty: {
+                        message: 'Vänligen fyll i ditt telefonnummer'
+                    }
+                }
+            },
+            lokaladress: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                        max: 100,
+                        message: 'Vänligen fyll i en giltig adress'
+                    },
+                }
+            },
+            datum: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                        max: 10,
+                        message: 'Vänligen fyll i ett giltigt datum'
+                    },
+                }
+            },
+            kurs: {
+                validators: {
+                    notEmpty: {
+                        message: 'Vänligen välj den kurs du är intresserad i'
+                    }
+                }
+            },
+            zip: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your zip code'
+                    },
+                    zipCode: {
+                        country: 'US',
+                        message: 'Please supply a vaild zip code'
+                    }
+                }
+            },
+            kommentar: {
+                validators: {
+                      stringLength: {
+                        min: 1,
+                        max: 200,
+                        message:'Vänligen skriv inte fler än 200 tecken'
+                    },
+                    }
+                }
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});
