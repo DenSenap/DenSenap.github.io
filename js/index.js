72d10e75566a24877478b7c989ff9cf471d3c264
densenap <32615739+DenMustard@users.noreply.github.com>
2017-11-18 01:27:06 +0100

    touch update

--- js/index.js
+++ js/index.js
@@ -144,3 +144,37 @@ $('.wrapper').find('a[href="#"]').on('click', function (e) {
     $(this).text(this.expand?"Click to collapse":"Click to read more");
     $(this).closest('.wrapper').find('.small, .big').toggleClass('small big');
 });
+
+function watchForHover() {
+    var hasHoverClass = false;
+    var container = document.body;
+    var lastTouchTime = 0;
+
+    function enableHover() {
+        // filter emulated events coming from touch events
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
