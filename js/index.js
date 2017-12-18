dc771af9aeb5f006cd4b200dba6003fbb123e4f7
densenap <32615739+DenMustard@users.noreply.github.com>
2017-12-18 02:35:23 +0100

    s

--- js/index.js
+++ js/index.js
@@ -83,7 +83,7 @@ $('a[href^="#"]').on('click', function(event) {
   if($target.length) {
     event.preventDefault();
     $('html, body').stop().animate({
-      scrollTop: $target.offset().top -100
+      scrollTop: $target.offset().top -75
     }, 750, 'easeInOutQuad');
   }
 });
