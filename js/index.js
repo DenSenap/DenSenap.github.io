5c8231ae571cccf7a46cb2ca0053957ca0dea510
densenap <32615739+DenMustard@users.noreply.github.com>
2018-05-04 18:20:13 +0200

    watchforhover moved

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
+function watchForHover() {
+    var hasHoverClass = false;
+    var container = document.body;
+    var lastTouchTime = 0;
+
+    function enableHover() {
+        // filter emulated events coming from touch events
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
+
+
+
 $(function() {
     $("#tabs").tabs();
 });
@@ -331,40 +367,6 @@ function updateTimer2() {
         '<div>' + s + '<span>Sekunder</span></div>';
 }
 setInterval('updateTimer2()', 1000);
-
-function watchForHover() {
-    var hasHoverClass = false;
-    var container = document.body;
-    var lastTouchTime = 0;
-
-    function enableHover() {
-        // filter emulated events coming from touch events
-        if (new Date() - lastTouchTime < 500) return;
-        if (hasHoverClass) return;
-
-        container.className += ' hasHover';
-        hasHoverClass = true;
-    }
-
-    function disableHover() {
-        if (!hasHoverClass) return;
-
-        container.className = container.className.replace(' hasHover', '');
-        hasHoverClass = false;
-    }
-
-    function updateLastTouchTime() {
-        lastTouchTime = new Date();
-    }
-
-    document.addEventListener('touchstart', updateLastTouchTime, true);
-    document.addEventListener('touchstart', disableHover, true);
-    document.addEventListener('mousemove', enableHover, true);
-
-    enableHover();
-}
-
-watchForHover();
 
 
 
